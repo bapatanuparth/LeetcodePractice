de9e40d0fa34ea807d9d3b23d134d3a8b3f71491
bapatanuparth <67226812+bapatanuparth@users.noreply.github.com>
2024-09-15 17:30:31 -0400

    9/15/2024

---
+++ Leetcode/src/leetcodeDynamicProgramming/FindingBookcaseShelves.java
@@ -0,0 +1,35 @@
+package leetcodeDynamicProgramming;
+
+//You are given an array books where books[i] = [thicknessi, heighti] indicates the thickness and height of the ith book. You are also given an integer shelfWidth.
+
+//
+//We want to place these books in order onto bookcase shelves that have a total width shelfWidth.
+//
+//We choose some of the books to place on this shelf such that the sum of their thickness is less than or equal to shelfWidth, then build another level of the shelf of the bookcase so that the total height of the bookcase has increased by the maximum height of the books we just put down. We repeat this process until there are no more books to place.
+//
+//Note that at each step of the above process, the order of the books we place is the same order as the given sequence of books.
+//
+//For example, if we have an ordered list of 5 books, we might place the first and second book onto the first shelf, the third book on the second shelf, and the fourth and fifth book on the last shelf.
+//Return the minimum possible height that the total bookshelf can be after placing shelves in this manner.
+
+public class FindingBookcaseShelves {
+	public int minHeightShelves(int[][] books, int shelfWidth) {
+		int dp[] = new int[books.length + 1];
+
+		dp[0] = 0;
+
+		for (int i = 1; i < dp.length; i++) {
+			int width = books[i - 1][0];
+			int height = books[i - 1][1];
+			dp[i] = dp[i - 1] + height;
+			for (int j = i - 1; j > 0 && width + books[j - 1][0] <= shelfWidth; j--) {
+				height = Math.max(height, books[j - 1][1]);
+				width += books[j - 1][0];
+				dp[i] = Math.min(dp[i], dp[j - 1] + height);
+			}
+		}
+
+		return dp[books.length];
+	}
+
+}
